9c01d83d8ea6b19ae8c82928dc52abc041cbb99a
markwallace-microsoft <mark.e.wallace@outlook.com>
2023-04-26 10:42:48 +0100

    Initial check-in
    - Update README and CONTRIBUTING
    - Add .github files
    - Placeholders for all initial starters
    - C# Hello World starter

---
+++ sk-csharp-hello-world/config/KernelSettings.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Text.Json.Serialization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+internal class KernelSettings
+{
+    public const string DefaultConfigFile = "config/appsettings.json";
+    public const string OpenAI = "OPENAI";
+    public const string AzureOpenAI = "AZUREOPENAI";
+
+    [JsonPropertyName("serviceType")]
+    public string ServiceType { get; set; } = string.Empty;
+
+    [JsonPropertyName("serviceId")]
+    public string ServiceId { get; set; } = string.Empty;
+
+    [JsonPropertyName("deploymentOrModelId")]
+    public string DeploymentOrModelId { get; set; } = string.Empty;
+
+    [JsonPropertyName("endpoint")]
+    public string Endpoint { get; set; } = string.Empty;
+
+    [JsonPropertyName("apiKey")]
+    public string ApiKey { get; set; } = string.Empty;
+
+    [JsonPropertyName("orgId")]
+    public string OrgId { get; set; } = string.Empty;
+
+    [JsonPropertyName("logLevel")]
+    public LogLevel? LogLevel { get; set; }
+
+    /// <summary>
+    /// Load the kernel settings from settings.json if the file exists and if not attempt to use user secrets.
+    /// </summary>
+    internal static KernelSettings LoadSettings()
+    {
+        if (File.Exists(DefaultConfigFile))
+        {
+            return FromFile(DefaultConfigFile);
+        }
+
+        return FromUserSecrets();
+    }
+
+    /// <summary>
+    /// Load the kernel settings from the specified configuration file if it exists.
+    /// </summary>
+    internal static KernelSettings FromFile(string configFile = DefaultConfigFile)
+    {
+        if (!File.Exists(configFile))
+        {
+            throw new FileNotFoundException($"Configuration not found: {configFile}");
+        }
+
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+            .AddJsonFile(configFile, optional: true, reloadOnChange: true)
+            .Build();
+
+        return configuration.Get<KernelSettings>()
+               ?? throw new InvalidDataException($"Invalid semantic kernel settings '{configFile}', please provide configuration settings using instructions in the README.");
+    }
+
+    /// <summary>
+    /// Load the kernel settings from user secrets.
+    /// </summary>
+    internal static KernelSettings FromUserSecrets()
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddUserSecrets<KernelSettings>()
+            .Build();
+
+        return configuration.Get<KernelSettings>()
+               ?? throw new InvalidDataException("Invalid semantic kernel settings, please provide configuration settings using instructions in the README.");
+    }
+}
---
+++ sk-python-hello-world/README.md
@@ -0,0 +1,1 @@
+# Semantic Kernel Python Hello World Starter
