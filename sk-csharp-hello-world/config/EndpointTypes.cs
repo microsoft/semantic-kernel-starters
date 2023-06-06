500e14eb207c4fa0ae2ef69515394a91cbbc660f
markwallace-microsoft <mark.e.wallace@outlook.com>
2023-06-06 17:09:00 +0100

    Update EndpointTypes.cs

--- sk-csharp-hello-world/config/EndpointTypes.cs
+++ sk-csharp-hello-world/config/EndpointTypes.cs
@@ -1,6 +1,4 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
-
-internal static class EndpointTypes
+﻿internal static class EndpointTypes
 {
     internal const string TextCompletion = "text-completion";
     internal const string ChatCompletion = "chat-completion";
