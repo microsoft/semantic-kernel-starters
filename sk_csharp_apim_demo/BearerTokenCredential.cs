122a60cfb77f25f29151dce56e52a6232285fc19
markwallace-microsoft <mark.e.wallace@outlook.com>
2023-06-07 16:29:51 +0100

    Add demo showing how to use interactive login with APIM

---
+++ sk_csharp_apim_demo/BearerTokenCredential.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Core;
+
+public class BearerTokenCredential : TokenCredential
+{
+    private readonly AccessToken _accessToken;
+
+    // Constructor that takes a Bearer token string and its expiration date
+    public BearerTokenCredential(AccessToken accessToken)
+    {
+        _accessToken = accessToken;
+    }
+
+    public override AccessToken GetToken(TokenRequestContext requestContext, CancellationToken cancellationToken)
+    {
+        return _accessToken;
+    }
+
+    public override ValueTask<AccessToken> GetTokenAsync(TokenRequestContext requestContext, CancellationToken cancellationToken)
+    {
+        return new ValueTask<AccessToken>(_accessToken);
+    }
+}
---
+++ sk_csharp_apim_demo/Program.cs
@@ -0,0 +1,38 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Orchestration;
+
+string[] scopes = new string[] { "https://cognitiveservices.azure.com/.default" };
+var credential = new InteractiveBrowserCredential();
+var requestContext = new TokenRequestContext(scopes);
+var accessToken = await credential.GetTokenAsync(requestContext);
+
+using ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder
+        .SetMinimumLevel(LogLevel.Warning)
+        .AddConsole()
+        .AddDebug();
+});
+
+IKernel kernel = new KernelBuilder()
+    .WithLogger(loggerFactory.CreateLogger<IKernel>())
+    .WithAzureTextCompletionService(
+        "text-davinci-003",
+        "https://apim...api.net/",
+        new BearerTokenCredential(accessToken)
+    )
+    .Build();
+
+var skillsDirectory = Path.Combine(System.IO.Directory.GetCurrentDirectory(), "skills");
+var skill = kernel.ImportSemanticSkillFromDirectory(skillsDirectory, "FunSkill");
+
+var context = new ContextVariables();
+context.Set("input", "Time travel to dinosaur age");
+context.Set("style", "Wacky");
+
+var result = await kernel.RunAsync(context, skill["Joke"]);
+
+Console.WriteLine(result);
