f2c354b75e041c63bc2b664323473d563c2167bb
Matthew Bolanos <matthewbolanos@gmail.com>
2023-07-14 23:47:31 +0100

    Initial commit of ChatGPT template

---
+++ sk-csharp-chatgpt-plugin/azure-function/Models/ApiKey.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Text.Json.Serialization;
+using Microsoft.Extensions.Configuration;
+
+namespace Models;
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+public class ApiKeySetting
+{
+    [JsonPropertyName("apiKey")]
+    public string ApiKey { get; set; } = string.Empty;
+}
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
