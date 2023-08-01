bb9688c345ffa28d56e5c043b721f9d92e3d625c
Mark Karle <mkarle@users.noreply.github.com>
2023-08-01 13:51:35 -0700

    Fall back to OpenAI if AzureOpenAI settings are not found

--- sk-python-flask-chatgpt-plugin/sk_python_flask_chatgpt_plugin/kernel_utils.py
+++ sk-python-flask-chatgpt-plugin/sk_python_flask_chatgpt_plugin/kernel_utils.py
@@ -30,12 +30,16 @@ def create_kernel_for_request(request_headers, skill_name):
     try:
         api_config = headers_to_config(request_headers)
     except ValueError:
-        logging.exception("No headers found. Using local .env file for configuration.")
+        logging.debug("No headers found. Using local .env file for configuration.")
         try:
             api_config = dotenv_to_config()
         except AssertionError:
-            logging.exception("No .env file found.")
-            return None, ("No valid headers found and no .env file found.", 400)
+            try:
+                logging.debug("No Azure OpenAI found in .env file.")
+                api_config = dotenv_to_config(use_azure_openai=False)
+            except AssertionError:
+                logging.debug("No valid .env file found.")
+                return None, ("No valid headers found and no .env file found.", 400)
 
     try:
         if (
@@ -66,7 +70,7 @@ def create_kernel_for_request(request_headers, skill_name):
             )
     except ValueError as e:
         logging.exception(f"Error creating completion service: {e}")
-        return None, ("Error creating completion service: {e}", 400)
+        return None, (f"Error creating completion service: {e}", 400)
 
     try:
         kernel.import_semantic_skill_from_directory(SKILLS_DIRECTORY, skill_name)
