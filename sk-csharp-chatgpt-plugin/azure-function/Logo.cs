e82aa67d08576628ce9d86edfaafca9a9ab0a609
Rafferty Uy <rafferty.uy@gmail.com>
2023-11-21 23:48:40 +0800

    fix dotnet format errors

--- sk-csharp-chatgpt-plugin/azure-function/Logo.cs
+++ sk-csharp-chatgpt-plugin/azure-function/Logo.cs
@@ -1,11 +1,6 @@
-using System.Net;
-using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
-using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using Models;
 
 public class Logo
 {
