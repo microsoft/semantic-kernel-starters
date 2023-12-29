a663508f6d676ed96ddf360a274f1432fe7ca865
ivanatilca <ivanatilca@hotmail.com>
2023-12-29 12:27:02 -0300

    Update to SK 1.0.1

--- sk-csharp-hello-world/config/ServiceCollectionExtensions.cs
+++ sk-csharp-hello-world/config/ServiceCollectionExtensions.cs
@@ -14,7 +14,7 @@ internal static class ServiceCollectionExtensions
         switch (kernelSettings.ServiceType.ToUpperInvariant())
         {
             case ServiceTypes.AzureOpenAI:
-                serviceCollection = serviceCollection.AddAzureOpenAIChatCompletion(kernelSettings.DeploymentId, kernelSettings.ModelId, endpoint: kernelSettings.Endpoint, apiKey: kernelSettings.ApiKey, serviceId: kernelSettings.ServiceId);
+                serviceCollection = serviceCollection.AddAzureOpenAIChatCompletion(kernelSettings.DeploymentId, endpoint: kernelSettings.Endpoint, apiKey: kernelSettings.ApiKey, serviceId: kernelSettings.ServiceId);
                 break;
 
             case ServiceTypes.OpenAI:
